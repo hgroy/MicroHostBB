e334d9edb8b16d1492334d65aa8bd580307d56c0
Harsh Gaurav <gharshry@gmail.com>
2025-10-30 14:45:10 +0530

    Point remotes to live Netlify endpoints

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,18 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index",
-  mode: "development",
+  mode: "production", // 👈 Important: change to production before deploying
+  output: {
+    publicPath: "auto",
+    clean: true,
+  },
   devServer: {
     static: path.join(__dirname, "dist"),
     port: 3005,
     historyApiFallback: true,
   },
-  output: {
-    publicPath: "auto",
-  },
   resolve: {
     extensions: [".js", ".jsx", ".json"],
-    // alias: {
-    //   hostapp: path.resolve(__dirname, 'path/to/hostapp'),
-    // },
   },
   module: {
     rules: [
@@ -25,30 +23,24 @@ module.exports = {
         test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-react"],
-        },
+        options: { presets: ["@babel/preset-react"] },
       },
       {
-        test: /\.css$/i, 
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "hostapp",
-      filename: "remoteEntry.js",
-
-    remotes: {
+      remotes: {
         chatapp: "chatapp@https://mellifluous-empanada-9bbc4a.netlify.app/remoteEntry.js",
         emailapp: "emailapp@https://guileless-baklava-86599f.netlify.app/remoteEntry.js",
-        },
-        
+      },
       exposes: {
         "./Card": "./src/components/Cards",
         "./Button": "./src/components/Button",
-    
       },
       shared: { react: { singleton: true }, "react-dom": { singleton: true } },
     }),
